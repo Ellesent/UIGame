49087c3892214a8cb588c4808769e495e91d050b
Ellesent <hypahflipperz@gmail.com>
2017-09-01 18:19:42 -0600

    created simple inventory system + added a pine tree

---
+++ UIGame/Assets/Scripts/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Inventory : MonoBehaviour {
+    List<string> inventory = new List<string>();
+
+    public GameObject inventoryImage;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddItem(string item)
+    {
+        inventory.Add(item);
+        AddImagetoInventory(item);
+    }
+
+    public void RemoveItem(string item)
+    {
+        inventory.Remove(item);
+    }
+
+    void AddImagetoInventory(string sprite)
+    {
+        Sprite x = Resources.Load<Sprite>("Sprites/pinetree");
+
+        for (int i = 0; i < inventoryImage.transform.childCount; i++)
+        {
+            if (inventoryImage.transform.GetChild(i).GetComponent<Image>().sprite == null)
+            {
+                inventoryImage.transform.GetChild(i).GetComponent<Image>().sprite = x;
+            }
+        }
+    }
+}
--- UIGame/Assets/Scripts/Player.cs
+++ UIGame/Assets/Scripts/Player.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
+    float speed = 5.0f;
+    public Inventory inventory;
+
+   
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,18 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Pickup")
+        {
+            inventory.AddItem(collision.gameObject.name);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    
 }
